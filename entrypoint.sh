e3f4da073a99fbe11428f961181db69b2d06c253
Brenda Wallace <brenda@wallace.net.nz>
2021-01-27 11:39:46 +1300

    read from env for github token

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,7 +29,7 @@ send_comment() {
 # mod_download is getting go modules using go.mod.
 mod_download() {
 	if [ ! -e go.mod ]; then go mod init; fi
-	git config --global url."https://${{ secrets.GITHUB_TOKEN }}:x-oauth-basic@github.com/".insteadOf "https://github.com/"
+	git config --global url."https://${{GITHUB_TOKEN}}:x-oauth-basic@github.com/".insteadOf "https://github.com/"
 	go mod download
 	if [ $? -ne 0 ]; then exit 1; fi
 }
